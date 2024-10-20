d06e985c58680b16e376614e3511909b4f1e0123
SeongMin Hwang <qq221qq@naver.com>
2024-10-20 14:52:51 +0900

    [INIT] 재고 감소 서비스 작성

--- stock/src/main/java/com/example/stock/StockApplication.java
+++ stock/src/main/java/com/example/stock/StockApplication.java
@@ -3,6 +3,10 @@ package com.example.stock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @SpringBootApplication
 public class StockApplication {
 
---
+++ stock/src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,28 @@
+package com.example.stock.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StockService {
+
+	private final StockRepository stockRepository;
+
+
+	@Transactional
+	public void decrease(Long id, Long quantity) {
+		Stock stock = stockRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("Stock not found"));
+
+		stock.decrease(quantity);
+		stockRepository.save(stock);
+	}
+}
